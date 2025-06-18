6ca46ea150b855e44078d7e0de193035b7daed4b
Primrose <primrose.njoki@student.moringaschool.com>
2025-06-18 09:04:55 +0300

    chatterbox

--- server/seed.py
+++ server/seed.py
@@ -1,34 +1,12 @@
-#!/usr/bin/env python3
-
-from random import choice as rc
-
-from faker import Faker
-
 from app import app
 from models import db, Message
 
-fake = Faker()
+with app.app_context():
+    db.drop_all()
+    db.create_all()
 
-usernames = [fake.first_name() for i in range(4)]
-if "Duane" not in usernames:
-    usernames.append("Duane")
+    m1 = Message(body="Hello world!", username="Ian")
+    m2 = Message(body="This is Flask!", username="Primrose")
 
-def make_messages():
-
-    Message.query.delete()
-    
-    messages = []
-
-    for i in range(20):
-        message = Message(
-            body=fake.sentence(),
-            username=rc(usernames),
-        )
-        messages.append(message)
-
-    db.session.add_all(messages)
-    db.session.commit()        
-
-if __name__ == '__main__':
-    with app.app_context():
-        make_messages()
+    db.session.add_all([m1, m2])
+    db.session.commit()
